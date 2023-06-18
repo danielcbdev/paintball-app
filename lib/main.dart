a02ea12ad38ed9ea950de65004cfd9e39bca4d5e
Daniel carvalho <danielcbdev@gmail.com>
2023-06-18 15:38:53 -0300

    configurando o firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:picospaintballzone/blocs_load.dart';
+import 'package:picospaintballzone/firebase_options.dart';
 import 'package:picospaintballzone/screens/splash/main.dart';
 
 void main() async {
 
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   runApp(
     BlocsLoad(
